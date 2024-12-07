e505f5a573a1da0cc12cfc0ee543853d30fa384b
FathimaShums <fathimashums416@gmail.com>
2024-12-07 08:10:35 +0530

    routes for Fooditem

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\FoodItemController;
+
+Route::get('/food-items', [FoodItemController::class, 'index']); // Get all food items
+Route::get('/food-items/{id}', [FoodItemController::class, 'show']); // Get a specific food item
+Route::post('/food-items', [FoodItemController::class, 'store']); // Create a new food item
+Route::put('/food-items/{id}', [FoodItemController::class, 'update']); // Update a food item
+Route::delete('/food-items/{id}', [FoodItemController::class, 'destroy']); // Delete a food item
 
 // Registration route
 Route::post('register', [AuthController::class, 'register']);
